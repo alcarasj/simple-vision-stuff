074c161d4e0f0eac9d5052a102eafc0c9671edfc
Jerico Alcaras <alcarasj@tcd.ie>
2018-10-15 13:49:56 +0100

    Added histogram back-projection.

---
+++ histogram_back_projection.py
@@ -0,0 +1,43 @@
+import numpy
+import cv2
+import sys
+from argparse import ArgumentParser
+
+COLOR = cv2.IMREAD_COLOR
+GRAYSCALE = cv2.IMREAD_GRAYSCALE
+UNCHANGED = cv2.IMREAD_UNCHANGED
+
+parser = ArgumentParser(description="Histogram back-projection.")
+parser.add_argument("-i", "--input", dest="input_path",
+					help="Input path for the image.")
+parser.add_argument("-o", "--output", dest="output_path",
+					help="Output path for the resulting image.")
+args = parser.parse_args()
+input_path = args.input_path
+output_path = args.output_path
+
+if not input_path or not output_path:
+	print("Specify input and output paths for images as arguments.")
+else:
+	input_image = cv2.imread(input_path)
+	roi = cv2.selectROI(input_image, fromCenter=False)
+	roi = input_image[int(roi[1]):int(roi[1] + roi[3]), int(roi[0]):int(roi[0] + roi[2])]
+	roi_hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+	input_hsv = cv2.cvtColor(input_image, cv2.COLOR_BGR2HSV)
+
+	roi_hist = cv2.calcHist([roi_hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])
+
+	cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+	dst = cv2.calcBackProject([input_hsv], [0, 1], roi_hist, [0, 180, 0, 256], 1)
+
+	disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+	cv2.filter2D(dst, -1, disc, dst)
+
+	ret, threshold = cv2.threshold(dst, 50, 255, 0)
+	threshold = cv2.merge((threshold, threshold, threshold))
+	result = cv2.bitwise_and(input_image, threshold)
+	result = numpy.vstack((input_image, threshold, result))
+	cv2.imwrite(output_path, result)
+	
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
--- vision.py
+++ vision.py
@@ -1,16 +1,25 @@
 import numpy
 import cv2
 import sys
+from argparse import ArgumentParser
 
 COLOR = cv2.IMREAD_COLOR
 GRAYSCALE = cv2.IMREAD_GRAYSCALE
 UNCHANGED = cv2.IMREAD_UNCHANGED
 
-if len(sys.argv) < 2:
-	print("Specify a path to an input image as an argument.")
+parser = ArgumentParser(description="Histogram back-projection.")
+parser.add_argument("-i", "--input", dest="input_path",
+					help="Input path for the image.")
+parser.add_argument("-o", "--output", dest="output_path",
+					help="Output path for the resulting image.")
+args = parser.parse_args()
+input_path = args.input_path
+output_path = args.output_path
+
+if not input_path or not output_path:
+	print("Specify input and output paths for images as arguments.")
 else:
-	image_path = sys.argv[1]
-	image = cv2.imread(image_path, GRAYSCALE)
-	cv2.imshow("image", image)
+	input_image = cv2.imread(input_path, GRAYSCALE)
+	cv2.imshow("image", input_image)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
